33fd8bcb91a56ab960e3b7dae8b864836818154f
Gray <grayzprogrammer@gmail.com>
2022-08-16 18:29:24 -0500

    creates a BannerAdUI View

--- AdMobAndCocoaPods/BannerAdUI.swift
+++ AdMobAndCocoaPods/BannerAdUI.swift
@@ -5,4 +5,30 @@
 //  Created by G Zhen on 8/16/22.
 //
 
-import Foundation
+import SwiftUI
+import GoogleMobileAds
+
+struct BannerAdUI: View {
+    @State private var adSize: CGSize = .zero
+    let adUnitID: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            BannerAdView(adUnitID: adUnitID)
+                .frame(width: adSize.width, height: adSize.height)
+                .onAppear {
+                    updateFrame()
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                    updateFrame()
+            }
+        }
+    }
+
+    private func updateFrame() {
+        let safeAreaInSets = UIApplication.shared.windows.first(where: {$0.isKeyWindow})?.safeAreaInsets ?? .zero
+        let frame = UIScreen.main.bounds.inset(by: safeAreaInSets)
+
+        adSize = GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(frame.width).size
+    }
+}
