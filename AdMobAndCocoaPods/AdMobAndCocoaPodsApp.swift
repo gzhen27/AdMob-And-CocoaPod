123870f0da4a4c6f26389227f1fed23d6ce24483
Gray <grayzprogrammer@gmail.com>
2022-08-16 09:46:45 -0500

    First Initial

---
+++ AdMobAndCocoaPods/AdMobAndCocoaPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdMobAndCocoaPodsApp.swift
+//  AdMobAndCocoaPods
+//
+//  Created by G Zhen on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AdMobAndCocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdMobAndCocoaPods/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdMobAndCocoaPods
+//
+//  Created by G Zhen on 8/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
