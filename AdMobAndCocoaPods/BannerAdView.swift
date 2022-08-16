15f03eab92722a7cdcade99da57c6d7bfc29886b
Gray <grayzprogrammer@gmail.com>
2022-08-16 17:01:11 -0500

    set up banner ad view

---
+++ AdMobAndCocoaPods/BannerAdView.swift
@@ -0,0 +1,24 @@
+//
+//  BannerAdView.swift
+//  AdMobAndCocoaPods
+//
+//  Created by G Zhen on 8/16/22.
+//
+
+import SwiftUI
+
+// conforms the BannerAdView to UIViewControllerRepresentable,
+// which to create and manage a UIViewController object in SwiftUI interface.
+final class BannerAdView: UIViewControllerRepresentable {
+    let adUnitID: String
+    
+    init(adUnitID: String) {
+        self.adUnitID = adUnitID
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        BannerAdViewController(adUnitID: adUnitID)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
---
+++ AdMobAndCocoaPods/BannerAdViewController.swift
@@ -0,0 +1,53 @@
+//
+//  BannerAdViewController.swift
+//  AdMobAndCocoaPods
+//
+//  Created by G Zhen on 8/16/22.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class BannerAdViewController: UIViewController {
+    let adUnitID: String
+    
+    // creates a view for banner ad
+    // for more detail: https://developers.google.com/admob/ios/banner
+    var bannerView = GADBannerView()
+    
+    init(adUnitID: String) {
+        self.adUnitID = adUnitID
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - lifecycles
+    override func viewDidLoad() {
+        // configure GADBannerView properties
+        bannerView.adUnitID = adUnitID
+        bannerView.rootViewController = self
+        
+        // adds the bannerView as a subview to the current view
+        view.addSubview(bannerView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
+    
+    private func loadAd() {
+        let frame = view.frame.inset(by: view.safeAreaInsets)
+        let width = frame.size.width
+        
+        // get adaptive GADAdsize and set the ad view
+        // for more detail: https://developers.google.com/admob/ios/banner/anchored-adaptive
+        bannerView.adSize = GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(width)
+        
+        bannerView.load(GADRequest())
+    }
+}
